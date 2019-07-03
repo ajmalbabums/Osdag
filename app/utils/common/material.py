d76cb34ec5a9cf4e0b0d6448c15d704551b33989
tanmaykalla <tanmaykalla95@gmail.com>
2019-07-03 19:55:15 +0530

    fub and fu updated

--- app/utils/common/material.py
+++ app/utils/common/material.py
@@ -1,7 +1,8 @@
 class Material(object):
 
-    def __init__(self, fy=0.0, fu=0.0):
-        self.fy = fy
+    def __init__(self, fyb=0.0, fub=0.0,fu=0):
+        self.fyb = fyb
+        self.fub = fub
         self.fu = fu
 
     def __repr__(self):
